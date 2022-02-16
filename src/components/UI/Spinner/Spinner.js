77a20fe446cdbdab926502c1b04d919f8ee3e649
Seth Bolander <bol15015@byui.edu>
2022-02-15 19:01:07 -0700

    Added loading spinner with state

---
+++ src/components/UI/Spinner/Spinner.js
@@ -0,0 +1,6 @@
+// from https://dev.to/kirteshbansal/spinner-loader-in-react-using-css-458h
+import classes from './Spinner.module.css';
+
+const Spinner = () => <div className={classes.Loader}><div></div></div>;
+
+export default Spinner;
